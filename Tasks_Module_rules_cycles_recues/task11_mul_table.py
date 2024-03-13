4e2d794c0a36e42694e9cff7725f32a39ec9ae5c
a.ershov <a.ershov@vega-gaz.ru>
2024-03-13 10:42:58 +0300

    task11_finish

--- Tasks_Module_rules_cycles_recues/task11_mul_table.py
+++ Tasks_Module_rules_cycles_recues/task11_mul_table.py
@@ -1,4 +1,22 @@
 print('Задача №11. Упорядоченный вывод данных')
 
-for i in range(10):
-    print(i+1, end="")
+def spc(dig):
+    if dig > 99:
+        return " "
+    if dig > 9:
+        return "  "
+    return "   "
+
+print("     ", end="")
+
+for name_col in range(1,11):
+    print(name_col, spc(name_col), end="")
+print()
+print()
+
+for row in range(1,11):
+    print(row, spc(row), end="")
+    for col in range(1,11):
+        vol = row*col
+        print(vol, spc(vol) , end="")
+    print()
