ee4949a416fa0e6a4e137be4accea824837e4dc8
larsanik <larsanik@gmail.com>
2024-04-10 15:37:05 +0300

    task_4_finish

--- Tasks_Modules_libs_files/task4_files_concat.py
+++ Tasks_Modules_libs_files/task4_files_concat.py
@@ -1,10 +1,20 @@
 def concat_files(*args):
-    for i in range(len(args)):
-        try:
-            with open(args[i], 'r', encoding='utf-8') as f:
-                print(f)
-        except:
-            print(f'Файл {args[i]} не найден!')
+    if args != ():
+        rc = open('res.txt', 'w', encoding='utf-8')
+        rc.close()
+        r = open('res.txt', 'a', encoding='utf-8')
+        for i in range(len(args)):
+            try:
+                with open(args[i], 'r', encoding='utf-8') as f:
+                    fr = f.read()
+                    r.write(fr)
+                    r.write('\n')
+            except:
+                print(f'Файл {args[i]} не найден!')
+        r.close()
+    else:
+        print('Ошибка! Программа запущена без аргументов!')
 
 
-concat_files('text1.txt', 'text2.txt', 'text3.txt', 'text4.txt')
+concat_files('text3.txt', 'text2.txt', 'text1.txt', 'text4.txt')
+# concat_files()
