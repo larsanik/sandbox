045172c170e8dac008d6b29bb12b1cfb9fc0bb96
larsanik <larsanik@gmail.com>
2024-04-10 14:54:21 +0300

    task_4_development_1

--- Tasks_Modules_libs_files/task4_files_concat.py
+++ Tasks_Modules_libs_files/task4_files_concat.py
@@ -1,6 +1,10 @@
 def concat_files(*args):
     for i in range(len(args)):
-        print(args[i])
+        try:
+            with open(args[i], 'r', encoding='utf-8') as f:
+                print(f)
+        except:
+            print(f'Файл {args[i]} не найден!')
 
 
 concat_files('text1.txt', 'text2.txt', 'text3.txt', 'text4.txt')
