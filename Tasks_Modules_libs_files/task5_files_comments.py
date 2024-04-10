9df1759a2f479a6e90179d894750a9ac133b5605
larsanik <larsanik@gmail.com>
2024-04-10 16:27:37 +0300

    task_5_development_1

--- Tasks_Modules_libs_files/task5_files_comments.py
+++ Tasks_Modules_libs_files/task5_files_comments.py
@@ -4,7 +4,7 @@ def del_comments(f_src, f_target):
         # открываем файлы
         with open(f_src, 'r', encoding='utf-8') as f:
             f_src_lines = f.readlines()
-        open(f_target, 'w', encoding='utf-8')
+        ft = open(f_target, 'w', encoding='utf-8')
     except:
         # обрабатываем ошибки
         print('Произошла ошибка. Программа будет завершена.')
@@ -12,13 +12,16 @@ def del_comments(f_src, f_target):
     else:
         # выполняем код по удалению комментариев
         for el in f_src_lines:
-
+            ft.write(el.split('#', 1)[0])
+            print(el)
+            print(el.split('#', 1)[0])
             ...
-    ...
+        ft.close()
 
 
 print('Программа удаляет комментарии из файла источника и записывает код в указанный файл.')
-f_src = input('Введите наименование файла источника: ')
-f_target = input('Введите наименование целевого файла: ')
+# f_src = input('Введите наименование файла источника: ')
+# f_target = input('Введите наименование целевого файла: ')
 
-del_comments(f_src, f_target)
+# del_comments(f_src, f_target)
+del_comments('task5_files_comments.py', 'no_comments.py')
