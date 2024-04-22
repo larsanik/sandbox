170c3b259b73d9c0f70819cfbfd1b4f8ddb26f4d
larsanik <larsanik@gmail.com>
2024-04-22 16:42:00 +0300

    test_colorama

---
+++ colorama_lib/color_test.py
@@ -0,0 +1,12 @@
+# from colorama import Fore, Back, Style
+# print(Fore.LIGHTGREEN_EX + 'some black text')
+# print(Back.BLACK + 'and with a green background')
+# print(Style.DIM + 'and in dim text')
+# #print(Back.MAGENTA + 'and with a green background')
+# print(Style.RESET_ALL)
+# print('back to normal now')
+
+from colorama import Fore, Back, Style
+print(Fore.RED + 'X' + Style.RESET_ALL)
+print(Fore.BLUE + 'O' + Style.RESET_ALL)
+print(Back.YELLOW + ' ' + Style.RESET_ALL)
