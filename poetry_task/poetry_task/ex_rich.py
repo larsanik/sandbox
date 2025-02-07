472e8dd4483785b94eb8228a84b8ece83f91e756
larsanik <larsanik@gmail.com>
2025-02-07 10:27:01 +0300

    poetry task

---
+++ poetry_task/poetry_task/ex_rich.py
@@ -0,0 +1,14 @@
+from rich.console import Console
+from rich.table import Table
+
+console = Console()
+
+table = Table(title="Пример таблицы")
+table.add_column("Имя", style="cyan", no_wrap=True)
+table.add_column("Фамилия", style="magenta")
+table.add_column("Возраст", justify="right", style="green")
+
+table.add_row("Анна", "Иванова", "25")
+table.add_row("Сергей", "Петров", "30")
+
+console.print(table)
